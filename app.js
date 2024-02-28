edfd73b984f846a5cf518ffce264b90d770303a4
RhiaFHunte <rhiafhunte@gmail.com>
2024-02-28 21:44:17 +0000

    Dynamic Character Counter Project
    I built and styled this basic project to add to my portfolio as an aspiring FE Dev!

---
+++ app.js
@@ -0,0 +1,10 @@
+const inputField = document.getElementById("input-field");
+const characterCountOutput = document.getElementById("character-count");
+
+inputField.addEventListener("input", (e) => {
+   const characterCount = e.target.value.length;
+   
+   characterCountOutput.innerText = characterCount;
+
+
+})
